72a31f37c496a6ba7cea46acab8ee1ea62a85235
Alessio <squitt00@yahoo.it>
2022-06-10 16:49:19 +0200

    + added documentation for ClashOfClansAPI class

--- src/main/java/com/alessio/coc/ClashOfClansAPI.java
+++ src/main/java/com/alessio/coc/ClashOfClansAPI.java
@@ -24,13 +24,24 @@ public class ClashOfClansAPI {
 	private Instant lastClanInfoUpdate = Instant.parse("2000-01-01T10:15:30.00Z");
 	private Instant warInfoUpdate = Instant.parse("2000-01-01T10:15:30.00Z");
 
-
+	/**
+	 * Initializes the class with the constants passed as argument.
+	 *
+	 * @param constants constants object.
+	 */
 	public ClashOfClansAPI(Constants constants) {
 		this.constants = constants;
 //		updateClanInfo();
 //		updateWarInfo();
 	}
 
+	/**
+	 * Builds and executes an HTTP GET request for general information
+	 * about the clan and its members.
+	 *
+	 * @return a {@code String} object containing the response
+	 *         of the request in JSON format.
+	 */
 	private String requestClanInfo() {
 		String url = "https://api.clashofclans.com/v1/clans/%23" + constants.getClanTag() + "/members";
 		String method = "GET";
@@ -38,6 +49,19 @@ public class ClashOfClansAPI {
 		return getResponse(Objects.requireNonNull(buildRequest(url, method)));
 	}
 
+	/**
+	 * Extracts general information about clan members from the string
+	 * passed as argument.
+	 * It also calls {@code extractOtherInfo(requestOtherInfo())} to get
+	 * additional information about clan members.
+	 * It puts all the data received in a {@code Clan} object.
+	 *
+	 * @param response the string containing the response
+	 *                 of the request in JSON format.
+	 *
+	 * @return a {@code Clan} object containing information
+	 *         about the clan members.
+	 */
 	public Clan extractClanInfo(String response) {
 		ArrayList<Member> members = new ArrayList<>();
 		JSONObject json = new JSONObject(response);
@@ -65,6 +89,15 @@ public class ClashOfClansAPI {
 		return new Clan(members);
 	}
 
+	/**
+	 * Builds and executes an HTTP GET request for additional information
+	 * about one clan member.
+	 *
+	 * @param dirtyTag the tag of the player to get information of.
+	 *
+	 * @return a {@code String} object containing the response
+	 *         of the request in JSON format.
+	 */
 	private String requestOtherInfo(String dirtyTag) {
 		String tag = dirtyTag.replace("#", "");
 		String url = "https://api.clashofclans.com/v1/players/%23" + tag;
@@ -73,6 +106,16 @@ public class ClashOfClansAPI {
 		return getResponse(Objects.requireNonNull(buildRequest(url, method)));
 	}
 
+	/**
+	 * Extracts additional information about a clan member from the string
+	 * passed as argument and puts it in a {@code HashMap<String, String>} object.
+	 *
+	 * @param response the string containing the response
+	 *                 of the request in JSON format.
+	 *
+	 * @return a {@code HashMap<String, String>} object containing
+	 *         the additional information about the clan member.
+	 */
 	private HashMap<String, String> extractOtherInfo(String response) {
 		HashMap<String, String> memberInfo = new HashMap<>();
 		JSONObject json = new JSONObject(response);
@@ -85,6 +128,13 @@ public class ClashOfClansAPI {
 		return memberInfo;
 	}
 
+	/**
+	 * Builds and executes an HTTP GET request for information
+	 * about the ongoing war.
+	 *
+	 * @return a {@code String} object containing the response
+	 *         of the request in JSON format.
+	 */
 	private String requestWarInfo() {
 		String url = "https://api.clashofclans.com/v1/clans/%23" + constants.getClanTag() + "/currentwar";
 		String method = "GET";
@@ -92,6 +142,18 @@ public class ClashOfClansAPI {
 		return getResponse(Objects.requireNonNull(buildRequest(url, method)));
 	}
 
+	/**
+	 * Extracts information about the ongoing war from the string
+	 * passed as argument and puts it in a {@code War} object.
+	 *
+	 * @param response the string containing the response
+	 *                 of the request in JSON format.
+	 *
+	 * @return a {@code War} object containing information
+	 *         about the ongoing war, that is:
+	 *         - general information about the war
+	 *         - performance of each member in war
+	 */
 	private War extractWarInfo(String response) {
 		JSONObject json = new JSONObject(response);
 		JSONObject ourClan = json.getJSONObject("clan");
@@ -133,6 +195,18 @@ public class ClashOfClansAPI {
 				members);
 	}
 
+	/**
+	 * Extracts information about the attacks of a player.
+	 * The information is contained in the {@code JSONArray} object.
+	 *
+	 * @param rawAttacks the object containing an array of
+	 *                   attacks in JSON format.
+	 * @param opponentsPositions the object mapping opponents
+	 *                           tags with their position in war.
+	 *
+	 * @return an {@code ArrayList<Attack>} object containing information
+	 *         about the attacks of a war member.
+	 */
 	private ArrayList<Attack> extractAttacks(JSONArray rawAttacks, HashMap<String, Integer> opponentsPositions) {
 		ArrayList<Attack> attacks = new ArrayList<>();
 
@@ -149,11 +223,30 @@ public class ClashOfClansAPI {
 		return attacks.size() > 1 ? orderAttacks(attacks) : attacks;
 	}
 
+	/**
+	 * Orders attacks of a member by the Attack object's parameter {@code getOrder}.
+	 *
+	 * @param attacks an {@code ArrayList<Attack>} object containing
+	 *                a list of attacks.
+	 *
+	 * @return an {@code ArrayList<Attack>} object containing the ordered
+	 *         list of attacks.
+	 */
 	private ArrayList<Attack> orderAttacks(ArrayList<Attack> attacks) {
 		attacks.sort(Comparator.comparing(Attack::getOrder));
 		return attacks;
 	}
 
+	/**
+	 * Extracts information about opponents from the parameter and Maps each
+	 * opponent tag with their position in war.
+	 *
+	 * @param opponentMembers an {@code JSONArray} object containing
+	 *                        an array of opponents in JSON format.
+	 *
+	 * @return a {@code HashMap<String, Integer>} object mapping opponents
+	 *         tags with their position in war.
+	 */
 	private HashMap<String, Integer> mapOpponents(JSONArray opponentMembers) {
 		HashMap<String, Integer> opponentsInfo = new HashMap<>();
 
@@ -164,6 +257,17 @@ public class ClashOfClansAPI {
 		return opponentsInfo;
 	}
 
+	/**
+	 * Builds an HTTP request with the URL and method passed as parameters.
+	 *
+	 * @param urlString a {@code String} object containing
+	 *                  the URL to use for the request.
+	 * @param method a {@code String} object containing
+	 *               the method to use for the response.
+	 *
+	 * @return an {@code HttpURLConnection} object with the
+	 *         specified parameters.
+	 */
 	private HttpURLConnection buildRequest(String urlString, String method) {
 		try {
 			URL url = new URL(urlString);
@@ -178,6 +282,15 @@ public class ClashOfClansAPI {
 		}
 	}
 
+	/**
+	 * Executes the request received as parameter and reads its input stream.
+	 * It then puts the content in a {@code String} object.
+	 *
+	 * @param request an {@code HttpURLConnection} object containing
+	 *                the request.
+	 *
+	 * @return a {@code String} object with the response in a JSON format.
+	 */
 	private String getResponse(HttpURLConnection request) {
 		StringBuilder content = new StringBuilder();
 		try {
@@ -196,10 +309,43 @@ public class ClashOfClansAPI {
 		return content.toString();
 	}
 
+	/**
+	 * Checks if the {@code Date} received as parameter is
+	 * older than {@code constants.getUpdateInterval()}.
+	 *
+	 * @param date the date od the last update to compare
+	 *             with the current time.
+	 *
+	 * @return the result of the comparison in a {@code boolean} variable.
+	 */
 	private boolean updateIsOld(Instant date) {
 		Duration timeElapsed = Duration.between(date, Instant.now());
 
 		return timeElapsed.toMinutes() > constants.getUpdateInterval();
+	}
+
+	/**
+	 * Updates the clan information after checking that
+	 * the last update is not too recent.
+	 * It then resets the last update time.
+	 */
+	public void updateClanInfo() {
+		if (updateIsOld(lastClanInfoUpdate)) {
+			this.clanInfo = extractClanInfo(requestClanInfo());
+			lastClanInfoUpdate = Instant.now();
+		}
+	}
+
+	/**
+	 * Updates the war information after checking that
+	 * the last update is not too recent.
+	 * It then resets the last update time.
+	 */
+	public void updateWarInfo() {
+		if (updateIsOld(warInfoUpdate)) {
+			this.warInfo = extractWarInfo(requestWarInfo());
+			warInfoUpdate = Instant.now();
+		}
 	}
 
 	public Clan getClanInfo() {
@@ -208,19 +354,5 @@ public class ClashOfClansAPI {
 
 	public War getWarInfo() {
 		return warInfo;
-	}
-
-	public void updateClanInfo() {
-		if (updateIsOld(lastClanInfoUpdate)) {
-			this.clanInfo = extractClanInfo(requestClanInfo());
-			lastClanInfoUpdate = Instant.now();
-		}
-	}
-
-	public void updateWarInfo() {
-		if (updateIsOld(warInfoUpdate)) {
-			this.warInfo = extractWarInfo(requestWarInfo());
-			warInfoUpdate = Instant.now();
-		}
 	}
 }
