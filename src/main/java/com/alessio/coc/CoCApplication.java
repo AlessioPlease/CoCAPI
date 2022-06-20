f4eabec0d8dbc28737544a16de22453583b6d40c
Alessio <squitt00@yahoo.it>
2022-06-20 20:27:06 +0200

    - removed unused inputs

--- src/main/java/com/alessio/coc/CoCApplication.java
+++ src/main/java/com/alessio/coc/CoCApplication.java
@@ -1,10 +1,8 @@
 package com.alessio.coc;
 
-import com.alessio.coc.models.*;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.util.ArrayList;
 
 @SpringBootApplication
 public class CoCApplication {
